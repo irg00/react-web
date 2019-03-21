ac348f5ab2dfffc649f4e902b9e32a22b7723bf0
Iñaki <irg00@iesemilidarder.com>
2019-03-21 11:01:46 +0100

    Se agrega el componente Blog, hay fallos con el ReactDom.

---
+++ js-sandbox/src/component/blog.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Blog extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Blog</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eveniet exercitationem ipsam necessitatibus officia quaerat soluta velit voluptatum. Et porro sint veritatis? Alias doloremque fugiat illo numquam, omnis pariatur temporibus ullam. Commodi consequatur consequuntur dignissimos facere iure nulla reiciendis similique.</p>
+            </div>
+        );
+    }
+
+}
+
+
+export default Blog;
+
--- js-sandbox/src/index.jsx
+++ js-sandbox/src/index.jsx
@@ -1,11 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './component/NavigationBar'
+import Blog from './component/blog'
 
 ReactDOM.render(
     <div>
         <NavBar/>
+    </div>,
+
+    <div>
+        <Blog/>
     </div>,
     document.getElementById('root')
 );
